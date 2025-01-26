f7730475940541f64f8982aa1cc03ad5739a9435
limited886@gmail.com <limited886@gmail.com>
2025-01-26 18:52:42 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      // localsConvention: 'camelCaseOnly'
+    plugins: [react()],
+    // base: 'https://palladiumv.github.io/createX/',
+    css: {
+        modules: {
+            // localsConvention: 'camelCaseOnly'
+        },
+        preprocessorOptions: {
+            css: {
+                alias: {
+                    '~': './node_modules',
+                },
+            },
+        },
     },
-    preprocessorOptions: {
-      css: {
-        alias: {
-          '~': './node_modules',
-        }
-      }
-    }
-  }
 })
